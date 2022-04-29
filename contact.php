e785937449d85fb24044d82debd16e15b7d46e54
Charlie <charlie@cloudbrk.io>
2022-04-28 20:25:09 -0400

    Contact form testing.

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $emailFrom = $_POST['emailFrom'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $companyName = $_POST['companyName'];
+    $connectType = $_POST['connectType'];
+    $emailBody = $_POST['emailBody'];
+    $fileUpload = $_POST['fileUpload'];
+}
+
+?>
